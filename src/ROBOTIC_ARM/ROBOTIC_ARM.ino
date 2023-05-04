b2fd72b5431f40af4d80687587042ae0116ae209
Mukund Gupta <73193030+mukundgupta-1@users.noreply.github.com>
2023-05-05 02:49:51 +0530

    added esp control for arm

--- src/ROBOTIC_ARM/ROBOTIC_ARM.ino
+++ src/ROBOTIC_ARM/ROBOTIC_ARM.ino
@@ -1,47 +1,106 @@
 #include <Servo.h>
+#include <ESP8266WiFi.h>
+
+/* define port */
+WiFiClient client;
+WiFiServer server(80);
+
+/* WIFI settings */
+const char* ssid = "coole";
+const char* password = "capstoneg47";
+
+/* data received from application */
+String  data = "";
+
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
 
 void setup() {
+  Serial.begin(115200);
+  delay(10);
   // Attach the servos to pins 9, 10, and 11
-  servo1.attach(9);
-  servo2.attach(10);
-  servo3.attach(11);
+  servo1.attach(2);
+  servo2.attach(4);
+  servo3.attach(16);
 
   // Set the initial position of the servos to 90 degrees
   servo1.write(165);
   servo2.write(0);
   servo3.write(130);
+  WiFi.begin(ssid, password);
 
-  // Initialize the serial communication
-  Serial.begin(9600);
+  // Attempt to connect to WiFi network:
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP  network:
+    // Wait 3 seconds for connection:
+    delay(3000);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+
+  /* start server communication */
+  server.begin();
 }
 
 void loop() {
-  // Check if there is any input from the serial port
-  if (Serial.available()) {
-    // Read the input character
-    char input = Serial.read();
+  client = server.available();
+  if (!client) return;
+  data = checkClient ();
 
-    // Move the arm to the first point
-    if (input == '1') {
-      servo1.write(60);
+  /******** Run function according to incoming data from application *********/
+
+  /* If the incoming data is "forward", run the "MotorForward" function */
+  if (data == "forward") MotorForward();
+  /* If the incoming data is "backward", run the "MotorBackward" function */
+  else if (data == "backward") MotorBackward();
+  /* If the incoming data is "left", run the "TurnLeft" function */
+  else if (data == "left") TurnLeft();
+  /* If the incoming data is "right", run the "TurnRight" function */
+  else if (data == "right") TurnRight();
+  /* If the incoming data is "stop", run the "MotorStop" function */
+  else if (data == "stop") MotorStop();
+}
+    void MotorForward(void)
+    {
+      servo1.write(0);
       servo2.write(0);
-      servo3.write(90);
+      servo3.write(30);
     }
     // Move the arm to the second point
-    else if (input == '2') {
-      servo1.write(75);
-      servo2.write(0);
-      servo3.write(105);
+    void MotorBackward(void){
+      servo1.write(45);
+      servo2.write(45);
+      servo3.write(30);
     }
     // Move the arm to the third point
-    else if (input == '3') {
-      servo1.write(90);
-      servo2.write(0);
-      servo3.write(90);
+    void TurnLeft(void) {
+      servo1.write(45);
+      servo2.write(45);
+      servo3.write(45);
     }
-  }
+  
+    void TurnRight(void){
+      servo1.write(45);
+      servo2.write(90);
+      servo3.write(45);
+    }
+    void MotorStop(void){
+      servo1.write(90);
+      servo2.write(90);
+      servo3.write(0);}
+
+      String checkClient (void)
+{
+  while (!client.available()) delay(1);
+  String request = client.readStringUntil('\r');
+  request.remove(0, 5);
+  request.remove(request.length() - 9, 9);
+  return request;
 }
